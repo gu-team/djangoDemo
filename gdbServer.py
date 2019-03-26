5ef05f0403513e155be257d2f3b88a82299075e1
douNine <bug@nuaa.edu.cn>
2019-03-26 17:20:56 +0800

    gdb server try

--- gdbServer.py
+++ gdbServer.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python
 import gdb
-import sys
-import os
-sys.path.append(r'~/Desktop/')
-
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoDemo.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-		    raise ImportError(
-				    "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(['gdbEntry.py', 'runserver'])
+import socket
 
 class GdbDemo():
     fileName = 'demo.c'
@@ -22,3 +8,37 @@ class GdbDemo():
         gdb.execute('file ' + self.fileName)
         ret = gdb.execute('start', to_string=True)
         return 'it is start return: ' + ret
+
+
+# TCP服务端
+def main():
+
+    #创建一个socket对象，AF_INET指定使用IPv4协议(AF_INET6代表IPV6)，SOCK_STREAM指定使用面向流的TCP协议
+    tcp_serve_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_serve_socket.bind(('127.0.01', 8001))   # 绑定本地ip和端口
+    tcp_serve_socket.listen(128)                # 开始监听端口，数字表示等待连接的最大数量
+
+    # 循环目的：多次调用accept为多个客户服务
+    while True:
+        print('等待一个新的客户端到来....')
+        tcp_client_socket, client_addr = tcp_serve_socket.accept()
+        print('一个新客户端%s到来' % str(client_addr))
+
+        # 循环目的：为同一个客户端服务多次
+        while True:
+            recv_data = tcp_client_socket.recv(1024)   # 从客户端接受消息，最多1024字节
+            print('客户端发来的是%s' % recv_data.decode('utf-8'))
+
+			# 用if判断当前用户是否还有需求，若没有（及无接收数据recv_data堵塞）则break退出当前循环
+            if not recv_data:
+                break
+            tcp_client_socket.send('hello welcome to gdb server'.encode('utf-8'))
+
+        # 关闭与客户端的连接
+        tcp_client_socket.close()
+        print('已经为该客户服务完毕')
+
+    tcp_serve_socket.close()
+
+if __name__ == '__main__':
+    main()
